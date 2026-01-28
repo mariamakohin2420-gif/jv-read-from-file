962c7237ecdf1b97b9f748af26539e8eef4f7e78
Mariia Makohin <maria.makohin2420@gmail.com>
2026-01-28 17:35:54 +0200

    First try

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,37 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
 public class FileWork {
-    public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+    public String[] readFromFile(String fileName) throws IOException {
+        File file = new File(fileName);
+        StringBuilder builder = new StringBuilder();
+        String[] line = new String[]{};
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            line = bufferedReader.readLine().split("\\W+");
+
+            while (line != null) {
+                for (String word : line) {
+                    if (word.toLowerCase().startsWith("w")) {
+                        builder.append(word);
+                    }
+
+                }
+                line = bufferedReader.readLine();
+
+
+            }
+            System.out.println(Arrays.toString(line));
+        } catch (RuntimeException e) {
+
+        }
+        return line;
     }
 }
